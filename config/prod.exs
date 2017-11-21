7e187681b4a836e7f4adfe3c169367b7fbe10f3a
Joel Klint <joel.klint@gmail.com>
2017-11-21 18:09:35 +0100

    Removed cache manifest from phoenix conf and removed database creation from heroku review apps

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :nexpo, Nexpo.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HOST_NAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/react_app/build/asset-manifest.json",
+  # cache_static_manifest: "priv/react_app/build/asset-manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
