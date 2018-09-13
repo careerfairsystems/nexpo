dd4fc4bb3a6c6b41f95af1c19db072dfe6178860
VictorWinberg <dat14vwi@student.lu.se>
2018-09-13 18:50:22 +0200

    Migrate to fix Category and CategoryAttribute

---
+++ priv/repo/migrations/20180913152108_fix_category.exs
@@ -0,0 +1,32 @@
+defmodule Nexpo.Repo.Migrations.FixCategory do
+  use Ecto.Migration
+
+  def change do
+    drop constraint(:company_entries, "company_entries_company_attribute_id_fkey")
+    drop constraint(:company_attributes, "company_attributes_pkey")
+    drop constraint(:company_attributes, "company_attributes_company_category_id_fkey")
+    drop constraint(:company_categories, "company_categories_pkey")
+    
+    rename table(:company_categories), to: table(:categories)
+    rename table(:company_attributes), to: table(:category_attributes)
+
+    alter table(:categories) do
+      modify :id, :integer, primary_key: true
+    end
+    alter table(:category_attributes) do
+      modify :id, :integer, primary_key: true
+    end
+    
+    rename table(:category_attributes), :company_category_id, to: :category_id
+    rename table(:company_entries), :company_attribute_id, to: :category_attribute_id
+    
+    alter table(:category_attributes) do
+      modify :category_id, references(:categories, on_delete: :delete_all)
+    end
+    alter table(:company_entries) do
+      modify :category_attribute_id, references(:category_attributes, on_delete: :delete_all)
+    end
+  end
+end
+
+# bigserial
