de61464e0e7fdf898e29fa4c95e986983386d134
antongoransson <anton.goransson95@gmail.com>
2018-09-19 15:07:16 +0200

    Remove unused import

--- priv/react_app/src/Screens/User/index.js
+++ priv/react_app/src/Screens/User/index.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import User from './User';
-import { Actions } from '../../Store';
 import { State } from '../../Store/reducers/index';
 
 const mapStateToProps = (state: State) => ({
