0db58c6d6afe56224c76e868893fe7cb76cb8325
Victor Winberg <victor.m.winberg@gmail.com>
2018-09-20 09:27:39 +0200

    Antd layout (#106)
    * Fix onTouchTap to onClick
    
    * Add Antd App Layout
    
    * Add logout test
    
    * Add tests for App.js

--- priv/react_app/src/App/index.js
+++ priv/react_app/src/App/index.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import App from './App';
 import { Actions } from '../Store';
 
-const mapStateToProps = (state, props) => ({
+const mapStateToProps = (state, { location, history }) => ({
   isLoggedIn: state.auth.isLoggedIn,
-  currentUser: state.entities.users[state.current.user] || {}
+  currentUser: state.entities.users[state.current.user] || {},
+  pathname: location.pathname,
+  redirect: history.push
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
