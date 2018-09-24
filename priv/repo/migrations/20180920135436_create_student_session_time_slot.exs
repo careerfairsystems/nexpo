9df7fe8b69f2b34f22e6e295d6e7343e1071f20e
Martin Johansson <martin.johansson1993@gmail.com>
2018-09-24 22:42:31 +0200

    Fixed problem with migrations, models and controller references to each other

--- priv/repo/migrations/20180920135436_create_student_session_time_slot.exs
+++ priv/repo/migrations/20180920135436_create_student_session_time_slot.exs
@@ -5,10 +5,11 @@ defmodule Nexpo.Repo.Migrations.CreateStudentSessionTimeSlot do
     create table(:student_session_time_slots) do
       add :start, :datetime
       add :end, :datetime
+      add :company_id, references(:companies, on_delete: :nothing)
       add :used, :boolean, default: false, null: false
 
       timestamps()
     end
-
+    create index(:student_session_time_slots, [:company_id])
   end
 end
