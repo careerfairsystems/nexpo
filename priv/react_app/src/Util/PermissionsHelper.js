644a9314a97adddefe964ad91c58ca77a5c23a76
antongoransson <anton.goransson95@gmail.com>
2018-09-26 14:40:01 +0200

    Add permissions for more routes

--- priv/react_app/src/Util/PermissionsHelper.js
+++ priv/react_app/src/Util/PermissionsHelper.js
@@ -5,7 +5,10 @@ const routePermissions = {
   categories: ['read_all', 'read_categories'],
   companies: ['read_all', 'read_companies'],
   roles: ['read_all', 'read_roles'],
-  users: ['read_all', 'read_users']
+  users: ['read_all', 'read_users'],
+  events: ['read_all', 'read_events'],
+  student_sessions: ['read_all', 'read_student_sessions'],
+  host_applications: ['read_all', 'read_host_applications']
 };
 
 const getBasePath = route => route.split('/').filter(i => i)[0];
