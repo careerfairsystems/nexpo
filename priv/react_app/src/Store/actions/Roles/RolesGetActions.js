501aa98a4049ce8323d9da69140c1c5a52371fa4
antongoransson <anton.goransson95@gmail.com>
2018-10-02 14:54:48 +0200

    Add messages for roles

--- priv/react_app/src/Store/actions/Roles/RolesGetActions.js
+++ priv/react_app/src/Store/actions/Roles/RolesGetActions.js
@@ -1,3 +1,4 @@
+import { message } from 'antd';
 import { actionTypes } from '../..';
 import API from '../../../API';
 
@@ -18,6 +19,9 @@ export type GetRoleFailureAction = {
   type: string
 };
 export function getRoleFailure(): GetRoleFailureAction {
+  message.error(
+    'Something went wrong when trying to fetch role, please try again later'
+  );
   return {
     type: actionTypes.FETCH_ROLE_FAILURE
   };
