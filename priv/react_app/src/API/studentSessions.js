4af88166dd6b267dd60d7bc8c14eb8d716a34a1a
antongoransson <anton.goransson95@gmail.com>
2018-10-03 12:25:40 +0200

    Add actions for delete student session appl

--- priv/react_app/src/API/studentSessions.js
+++ priv/react_app/src/API/studentSessions.js
@@ -2,12 +2,18 @@
 *   This file contains methods to access the /student_sessions resource on the server.
 */
 
-import { authPost, handleHttpResponse } from './utils';
+import { authPost, authDelete, handleHttpResponse } from './utils';
 
 export default {
   /**
    * Create a student session application
    */
   create: data =>
-    authPost('/api/student_session_applications', data).then(handleHttpResponse)
+    authPost('/api/student_session_applications', data).then(
+      handleHttpResponse
+    ),
+  destroy: id =>
+    authDelete(`/api/me/student_session_applications/${id}`).then(
+      handleHttpResponse
+    )
 };
