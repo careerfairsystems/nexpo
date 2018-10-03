6c41042a039ad7adf03af03f8de7b9d16794b88d
antongoransson <anton.goransson95@gmail.com>
2018-10-03 15:46:45 +0200

    Add tests for reducer

--- priv/react_app/src/Store/reducers/Entities/entitiesTestData.js
+++ priv/react_app/src/Store/reducers/Entities/entitiesTestData.js
@@ -107,6 +107,9 @@ const testData = {
       }
     ]
   },
+  sessionApplications: {
+    1: { id: 1, companyId: 1, studentId: 1, motivation: 'Old motivation' }
+  },
   categories: [
     {
       title: 'Logistik',
