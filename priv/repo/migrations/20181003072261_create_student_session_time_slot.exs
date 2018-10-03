432161ced0fea86e531d0e6b887b22ad00fd36cf
VictorWinberg <victor.m.winberg@gmail.com>
2018-10-03 09:31:27 +0200

    Rename migrations

--- priv/repo/migrations/20181003072261_create_student_session_time_slot.exs
+++ priv/repo/migrations/20181003072261_create_student_session_time_slot.exs
@@ -3,8 +3,8 @@ defmodule Nexpo.Repo.Migrations.CreateStudentSessionTimeSlot do
 
   def change do
     create table(:student_session_time_slots) do
-      add :start, :datetime
-      add :end, :datetime
+      add :start, :date
+      add :end, :date
       add :used, :boolean, default: false, null: false
       add :company_id, references(:companies, on_delete: :nothing)
 
@@ -13,4 +13,3 @@ defmodule Nexpo.Repo.Migrations.CreateStudentSessionTimeSlot do
     create index(:student_session_time_slots, [:company_id])
   end
 end
- 
--- web/models/student_session_time_slot.ex
+++ web/models/student_session_time_slot.ex
@@ -2,14 +2,14 @@ defmodule Nexpo.StudentSessionTimeSlot do
   use Nexpo.Web, :model
 
   schema "student_session_time_slots" do
-    field :start, Ecto.DateTime
-    field :end, Ecto.DateTime
+    field :start, Ecto.Date
+    field :end, Ecto.Date
     field :used, :boolean, default: false
 
     belongs_to :company, Nexpo.Company
 
     timestamps()
-    
+
   end
 
   @doc """
