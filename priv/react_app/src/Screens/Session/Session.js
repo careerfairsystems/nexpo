01c7a4f0edf9e50425b5434a30d1267a6884a3f2
Victor Winberg <victor.m.winberg@gmail.com>
2018-10-03 15:48:41 +0200

    Fix session view (#186)
    * Fix Companies table fixed
    
    * Check description size
    
    * Update Session screen

--- priv/react_app/src/Screens/Session/Session.js
+++ priv/react_app/src/Screens/Session/Session.js
@@ -5,16 +5,38 @@ class Session extends Component {
   render() {
     return (
       <div>
-        <InvisibleLink to="session/application">Apply</InvisibleLink>
+        <h1>Welcome to ARKAD - Student Session!</h1>
+        <h3>How to apply for Student Sessions:</h3>
+        <h4>
+          <li>
+            Go to your <InvisibleLink to="/user"> profile </InvisibleLink> and
+            fill in your phone number and upload your CV(s).
+          </li>
+          <li>
+            Go to
+            <InvisibleLink to="/session/application"> apply </InvisibleLink>
+            and choose a company. Write a short motivation and submit your
+            application.
+          </li>
+          <br />
+          Note: If you apply for more than one company they will all receive the
+          same CV(s) but different motivations. You can view all your
+          applications
+          <InvisibleLink to="/session/applications"> here</InvisibleLink>.
+        </h4>
         <br />
-        <InvisibleLink to="session/applications">
-          View your applications
-        </InvisibleLink>
         <br />
-        <InvisibleLink to="session/companies">
-          View available companies
-        </InvisibleLink>
+        <h3>Do you want to know more about the companies?</h3>
+        <h4>
+          <InvisibleLink to="/session/companies">Go to Companies</InvisibleLink>
+        </h4>
         <br />
+        <br />
+        <h3>Do you have any questions about the student sessions?</h3>
+        <h4>
+          Contact:
+          <a href="mailto:company.arkad@tlth.se"> company.arkad@tlth.se </a>
+        </h4>
       </div>
     );
   }
