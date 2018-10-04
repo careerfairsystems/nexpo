7dac6562b18223b123857745c6bdf5a1f39a6012
antongoransson <anton.goransson95@gmail.com>
2018-10-04 16:34:40 +0200

    Replace material-ui in forgotpassword

--- priv/react_app/src/Components/ForgotPasswordEnterEmail/ForgotPasswordEnterEmail.test.js
+++ priv/react_app/src/Components/ForgotPasswordEnterEmail/ForgotPasswordEnterEmail.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
+import Button from 'antd/lib/button';
+import Input from 'antd/lib/input';
 import ForgotPasswordEnterEmail from './ForgotPasswordEnterEmail';
+import { SuccessMessage } from '../SuccessMessage/SuccessMessage';
 
 it('can render without crashing', () => {
   const props = {
@@ -11,12 +12,21 @@ it('can render without crashing', () => {
   shallow(<ForgotPasswordEnterEmail {...props} />);
 });
 
+it('renders SuccessMessage on success', () => {
+  const props = {
+    callBackend: jest.fn(),
+    success: true
+  };
+  const wrapper = shallow(<ForgotPasswordEnterEmail {...props} />);
+  expect(wrapper.find(SuccessMessage)).toHaveLength(1);
+});
+
 it('renders an input field', () => {
   const props = {
     callBackend: jest.fn()
   };
   const wrapper = shallow(<ForgotPasswordEnterEmail {...props} />);
-  expect(wrapper.find(TextField)).toHaveLength(1);
+  expect(wrapper.find(Input)).toHaveLength(1);
 });
 
 it('renders a button', () => {
@@ -24,5 +34,40 @@ it('renders a button', () => {
     callBackend: jest.fn()
   };
   const wrapper = shallow(<ForgotPasswordEnterEmail {...props} />);
-  expect(wrapper.find(RaisedButton)).toHaveLength(1);
+  expect(wrapper.find(Button)).toHaveLength(1);
+});
+
+it('input correctly updates state', () => {
+  const props = {
+    callBackend: jest.fn()
+  };
+  const email = 'dev@it';
+  const wrapper = shallow(<ForgotPasswordEnterEmail {...props} />);
+  expect(wrapper.state('email')).toEqual('');
+  wrapper
+    .find(Input)
+    .props()
+    .onChange({ target: { value: email } });
+  expect(wrapper.state('email')).toEqual(email);
+});
+
+it('calls callBackend on button click and input enter press', () => {
+  const props = {
+    callBackend: jest.fn()
+  };
+  const wrapper = shallow(<ForgotPasswordEnterEmail {...props} />);
+  const email1 = 'dev@it';
+  const email2 = 'test@it';
+  wrapper.setState({ email: email1 });
+  expect(props.callBackend).toHaveBeenCalledTimes(0);
+  wrapper.find(Button).simulate('click');
+  expect(props.callBackend).toHaveBeenCalledTimes(1);
+  expect(props.callBackend).lastCalledWith({ email: email1 });
+  wrapper.setState({ email: email2 });
+  wrapper
+    .find(Input)
+    .props()
+    .onPressEnter();
+  expect(props.callBackend).toHaveBeenCalledTimes(2);
+  expect(props.callBackend).lastCalledWith({ email: email2 });
 });
