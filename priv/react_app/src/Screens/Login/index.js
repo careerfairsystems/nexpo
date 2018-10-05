302803f0169fa998db5f73bd51d1375cf897f51e
antongoransson <anton.goransson95@gmail.com>
2018-10-05 11:40:00 +0200

    Restore line changed for development

--- priv/react_app/src/Screens/Login/index.js
+++ priv/react_app/src/Screens/Login/index.js
@@ -6,6 +6,6 @@
 const Login =
   process.env.NODE_ENV === 'production'
     ? require('./ProductionLogin').default
-    : require('./ProductionLogin').default;
+    : require('./DevelopmentLogin').default;
 
 export default Login;
