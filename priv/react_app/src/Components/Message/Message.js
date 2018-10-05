60ba5e4154b2aa4f4058a949f306c921dda55554
antongoransson <anton.goransson95@gmail.com>
2018-10-05 11:56:34 +0200

    Replace material-ui in message componetn

--- priv/react_app/src/Components/Message/Message.js
+++ priv/react_app/src/Components/Message/Message.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AlertError from 'material-ui/svg-icons/alert/error';
-import { red500 } from 'material-ui/styles/colors';
-
-import ActionCheckCirle from 'material-ui/svg-icons/action/check-circle';
-import { green500 } from 'material-ui/styles/colors';
+import { Icon } from 'antd';
 
 import { Link } from 'react-router-dom';
 import './Message.css';
@@ -14,14 +10,31 @@ import './Message.css';
  * A Component which renders a message
  * - Consists of an icon, text, and a link
  */
+const iconStyle = {
+  height: '200px',
+  width: 'auto',
+  fontSize: 200
+};
+const styles = {
+  error: {
+    ...iconStyle,
+    color: '#f44336'
+  },
+  success: {
+    ...iconStyle,
+    color: '#4caf50'
+  }
+};
 const Message = ({ message, linkUrl, linkText, type }) => {
   let icon = null;
   switch (type) {
     case 'error':
-      icon = <AlertError color={red500} style={styles.icon} />;
+      icon = (
+        <Icon type="exclamation-circle" theme="filled" style={styles.error} />
+      );
       break;
     case 'success':
-      icon = <ActionCheckCirle color={green500} style={styles.icon} />;
+      icon = <Icon type="check-circle" theme="filled" style={styles.success} />;
       break;
     default:
       icon = '';
@@ -38,11 +51,9 @@ const Message = ({ message, linkUrl, linkText, type }) => {
   );
 };
 
-const styles = {
-  icon: {
-    height: '200px',
-    width: 'auto'
-  }
+Message.defaultProps = {
+  linkUrl: null,
+  linkText: ''
 };
 
 Message.propTypes = {
