76a4bb602312cddd828479ae24a2af5077ba1976
Anton Göransson <anton.goransson95@gmail.com>
2018-10-05 08:02:18 +0200

    Cleanup in forms (#198)
    * Replace material-ui in forgotpassword
    
    * Add signup form
    
    * Add replacecpassword form
    
    * Move submissionerror to action
    
    * Add disabled when submitting for forms
    
    * Cleanup in forms and add disabled when submitting

--- priv/react_app/src/Components/Forms/StudentForm.js
+++ priv/react_app/src/Components/Forms/StudentForm.js
@@ -12,7 +12,8 @@ const StudentForm = ({
   action,
   currentStudent,
   onRemove,
-  fileList
+  fileList,
+  submitting
 }) => (
   <Form onSubmit={handleSubmit}>
     <Field
@@ -37,7 +38,7 @@ const StudentForm = ({
       onRemove={onRemove}
     />
 
-    <Button disabled={disabled} htmlType="submit">
+    <Button disabled={disabled || submitting} htmlType="submit">
       Save CV(s)
     </Button>
   </Form>
@@ -50,7 +51,7 @@ StudentForm.defaultProps = {
 StudentForm.propTypes = {
   disabled: PropTypes.bool,
   handleSubmit: PropTypes.func.isRequired,
-  reset: PropTypes.func.isRequired
+  submitting: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
