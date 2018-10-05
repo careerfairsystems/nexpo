bdbfe09f38a58da020792d4b7bb1f4cdb3a8800a
Anton Göransson <anton.goransson95@gmail.com>
2018-10-05 18:36:37 +0200

    Update constraints for company and student-session (#202)
    * Update constraints
    
    * Fix copypasta
    
    * Shouldn't it look like this?
    
    * While im on it
    
    * Another indent

---
+++ priv/repo/migrations/20181005115709_update-student-session-constraint.exs
@@ -0,0 +1,15 @@
+defmodule Nexpo.Repo.Migrations.UpdateStudentSessionConstraint do
+  use Ecto.Migration
+
+  def change do
+    drop constraint(:student_sessions, "student_sessions_company_id_fkey")
+    drop constraint(:student_session_applications, "student_session_applications_company_id_fkey")
+
+    alter table(:student_sessions) do
+      modify :company_id, references(:companies, on_delete: :nilify_all)
+    end
+    alter table(:student_session_applications) do
+      modify :company_id, references(:companies, on_delete: :nilify_all)
+    end
+  end
+end
