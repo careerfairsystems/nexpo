9c79548dd8ec428c6fbffe383e047c03f35090c1
antongoransson <anton.goransson95@gmail.com>
2018-10-08 14:21:01 +0200

    Add logic for deleting cv

--- web/controllers/student_controller.ex
+++ web/controllers/student_controller.ex
@@ -3,6 +3,7 @@ defmodule Nexpo.StudentController do
   use Guardian.Phoenix.Controller
 
   alias Nexpo.Student
+  alias Nexpo.{CvSv, CvEn}
   alias Guardian.Plug.{EnsurePermissions}
 
   plug EnsurePermissions, [handler: Nexpo.SessionController,
@@ -54,13 +55,16 @@ defmodule Nexpo.StudentController do
         conn
         |> put_status(:unprocessable_entity)
         |> render(Nexpo.ChangesetView, "error.json", changeset: changeset)
+      end
     end
-  end
+
 
   def update_student(conn, %{"student" => student_params}, user, _claims) do
     student = Repo.get_by!(Student, %{user_id: user.id})
     changeset = Student.changeset(student, student_params)
-
+    Map.keys(changeset.changes)
+      |> Enum.filter(fn attr -> attr == :resume_sv_url or attr == :resume_en_url end)
+      |> Enum.each(fn cv -> update_cv(student, changeset, cv) end)
     case Repo.update(changeset) do
       {:ok, student} ->
         render(conn, "show.json", student: student)
@@ -79,5 +83,16 @@ defmodule Nexpo.StudentController do
     Repo.delete!(student)
 
     send_resp(conn, :no_content, "")
+  end
+
+  defp update_cv(student, changeset, cv) do
+    cv_file = Map.get(student, cv)
+    # This means that we recently sent null to indicate file removal
+    if Map.get(changeset, cv) == nil and  cv_file != nil do
+      case cv do
+        :resume_sv_url -> CvSv.delete({cv_file, student})
+        :resume_en_url -> CvEn.delete({cv_file, student})
+      end
+    end
   end
 end
