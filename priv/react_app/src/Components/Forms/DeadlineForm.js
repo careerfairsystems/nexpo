d252c5f3889a911f944a9740357f3b8ed59cd83c
VictorWinberg <victor.m.winberg@gmail.com>
2018-10-09 11:04:34 +0200

    Add MyDatePicker

--- priv/react_app/src/Components/Forms/DeadlineForm.js
+++ priv/react_app/src/Components/Forms/DeadlineForm.js
@@ -2,31 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import { Button, Form, Input, DatePicker } from 'antd';
-import moment from 'moment';
+import { Button, Form, Input } from 'antd';
+import DatePicker from '../DatePicker';
+
 import makeField from './helper';
 
 const TextInput = makeField(Input);
-const DatePick = makeField(DatePicker);
+const MyDatePicker = makeField(DatePicker);
 
 const DeadlineForm = ({ handleSubmit }) => (
   <Form onSubmit={handleSubmit}>
     <Field name="name" label="Name:" component={TextInput} />
-    <Field
-      name="start"
-      label="Start Date:"
-      defaultValue={moment('2015/01/01', 'YYYY/MM/DD')}
-      format="YYYY/MM/DD"
-      component={DatePick}
-    />
-    <Field
-      name="end"
-      label="End Date:"
-      defaultValue={moment('2015/01/01', 'YYYY/MM/DD')}
-      format="YYYY/MM/DD"
-      component={DatePick}
-    />
-    <Button htmlType="submit">Create deadline</Button>
+    <Field name="start" label="Start Time:" component={MyDatePicker} />
+    <Field name="end" label="End Time:" component={MyDatePicker} />
+    <Button htmlType="submit">Submit</Button>
   </Form>
 );
 
