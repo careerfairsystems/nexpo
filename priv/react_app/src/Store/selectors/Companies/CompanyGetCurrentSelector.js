6697d8d6fc14509507bf92c6c88b8aba24865c24
Viktor Claesson <claesson.viktor@gmail.com>
2018-10-10 14:16:26 +0200

    no longer crashes for non representives

--- priv/react_app/src/Store/selectors/Companies/CompanyGetCurrentSelector.js
+++ priv/react_app/src/Store/selectors/Companies/CompanyGetCurrentSelector.js
@@ -1,12 +1,18 @@
+import {isNil} from 'lodash/fp';
 import users from '../Users';
 
 export const getCurrentCompany = state => {
   const currentUser = users.getCurrentUser(state);
   const representativeId = currentUser.representative;
-  const representative = state.entities.representatives[representativeId];
-  const company = state.entities.companies[representative.company] || {};
 
-  return { ...company };
+  if(isNil(representativeId)) {
+    const representative = state.entities.representatives[representativeId];
+    const company = state.entities.companies[representative.company] || {};
+
+    return { ...company };
+  } else {
+    return {};
+  }
 };
 
 export default { getCurrentCompany };
