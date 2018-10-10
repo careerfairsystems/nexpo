f5afe3684e189e29c4277178dff068af5dc4a39c
saammirghorbani <95sami81@gmail.com>
2018-10-10 12:35:54 +0200

    Delete roles

--- priv/react_app/src/Screens/Admin/Roles/Roles.js
+++ priv/react_app/src/Screens/Admin/Roles/Roles.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import Popconfirm from 'antd/lib/popconfirm';
+import { sortBy } from 'lodash/fp';
 import Table from 'antd/lib/table';
 import Button from 'antd/lib/button';
 import Divider from 'antd/lib/divider';
 import InvisibleLink from '../../../Components/InvisibleLink';
 import LoadingSpinner from '../../../Components/LoadingSpinner';
 import HtmlTitle from '../../../Components/HtmlTitle';
-
-const roleColumns = [
-  {
-    title: 'Type',
-    dataIndex: 'type',
-    key: 'type',
-    render: (type, { id }) => (
-      <InvisibleLink to={`/admin/roles/${id}`}>{type}</InvisibleLink>
-    )
-  },
-  {
-    title: 'Permissions',
-    dataIndex: 'permissions',
-    key: 'permissions',
-    render: permissions => <span>{permissions.join(', ')}</span>
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: role => (
-      <span>
-        <InvisibleLink to={`/admin/roles/${role.id}`}>Show</InvisibleLink>
-        <Divider type="vertical" />
-        <InvisibleLink to={`/roles/${role.id}/#role-edit`}>Edit</InvisibleLink>
-        <Divider type="vertical" />
-        <InvisibleLink to="#role-delete">Delete</InvisibleLink>
-      </span>
-    )
-  }
-];
 
 /**
  * Responsible for rendering a list of roles
@@ -46,6 +17,43 @@ class Roles extends Component {
     const { getAllRoles } = this.props;
     getAllRoles();
   }
+
+  roleColumns = () => [
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+      render: (type, { id }) => (
+        <InvisibleLink to={`/admin/roles/${id}`}>{type}</InvisibleLink>
+      )
+    },
+    {
+      title: 'Permissions',
+      dataIndex: 'permissions',
+      key: 'permissions',
+      render: permissions => <span>{permissions.join(', ')}</span>
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: role => (
+        <span>
+          <InvisibleLink to={`/admin/roles/${role.id}`}>Show</InvisibleLink>
+          <Divider type="vertical" />
+          <InvisibleLink to={`/admin/roles/${role.id}/#role-edit`}>
+            Edit
+          </InvisibleLink>
+          <Divider type="vertical" />
+          <Popconfirm
+            title="Are you sure you want to delete this role?"
+            onConfirm={() => this.props.deleteRole(role.id)}
+          >
+            <span style={{ color: '#ff4d4f', cursor: 'pointer' }}>Delete</span>
+          </Popconfirm>
+        </span>
+      )
+    }
+  ];
 
   renderRoles() {
     const { roles } = this.props;
@@ -57,11 +65,14 @@ class Roles extends Component {
         <h1>Roles</h1>
 
         <Table
-          columns={roleColumns}
-          dataSource={Object.keys(roles).map(i => ({
-            ...roles[i],
-            key: i
-          }))}
+          columns={this.roleColumns()}
+          dataSource={sortBy(
+            'type',
+            Object.keys(roles).map(i => ({
+              ...roles[i],
+              key: i
+            }))
+          )}
         />
 
         <Button onClick={() => null} type="primary">
