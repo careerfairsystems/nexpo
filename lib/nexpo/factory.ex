fd7c603e94748d80b437993958d4d942e33ea732
VictorWinberg <dat14vwi@student.lu.se>
2018-10-16 20:14:31 +0200

    Update lib nexpo

--- lib/nexpo/factory.ex
+++ lib/nexpo/factory.ex
@@ -90,8 +90,7 @@ defmodule Nexpo.Factory do
            |> Map.put(:signup_key, user.signup_key)
            |> Nexpo.User.final_signup!
     Nexpo.Repo.get!(Nexpo.User, user.id)
-    |> Nexpo.Repo.preload(:student)
-    |> Nexpo.Repo.preload(:representative)
+    |> Nexpo.Repo.preload([:student, :representative])
   end
 
   @doc """
