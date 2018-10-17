41bba924efe6a47c26316d0e188bcb185bb514f7
VictorWinberg <victor.m.winberg@gmail.com>
2018-10-17 17:35:21 +0200

    Fix tests

--- priv/react_app/src/Screens/Admin/Programme/Programme.test.js
+++ priv/react_app/src/Screens/Admin/Programme/Programme.test.js
@@ -2,46 +2,78 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Programme from './Programme';
 import NotFound from '../../NotFound';
-// import HtmlTitle from '../../../Components/HtmlTitle';
+import LoadingSpinner from '../../../Components/LoadingSpinner';
+import ProgrammeForm from '../../../Forms/ProgrammeForm';
 
-it('should render without crashing', () => {
-  const programme = {};
-  const func = jest.fn();
-  shallow(<Programme id="1" programme={programme} getProgramme={func} updateProgramme={func} />);
-});
+describe('Programme', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      programme: {},
+      fetching: false,
+      getProgramme: jest.fn(),
+      createProgramme: jest.fn(),
+      updateProgramme: jest.fn()
+    };
+  });
 
-it('should render NotFound if there is no programme', () => {
-  const programme = {};
-  const func = jest.fn();
-  const wrapper = shallow(
-    <Programme id="1" programme={programme} getProgramme={func} updateProgramme={func} />
-  );
+  it('should render without crashing', () => {
+    const programme = {
+      name: 'D-Guild',
+      code: 'D'
+    };
+    shallow(<Programme id="1" {...props} programme={programme} />);
+  });
 
-  expect(wrapper.find(NotFound)).toHaveLength(1);
-});
+  it('should render loading while fetching ', () => {
+    const wrapper = shallow(<Programme id="1" {...props} fetching />);
 
-// it('should set html title', () => {
-//   const programme = {
-//     name: 'Dev'
-//   };
-//   const func = jest.fn();
-//   const wrapper = shallow(<Programme id="1" programme={programme} getProgramme={func} updateProgramme={func} />);
-//
-//   expect(
-//     wrapper.contains(<HtmlTitle title={capitalize(programme.name)} />)
-//   ).toBeTruthy();
-// });
+    expect(wrapper.find(LoadingSpinner)).toHaveLength(1);
+  });
 
-it('should render programme information', () => {
-  const programme = {
-    email: 'dev@it',
-    firstName: 'Dev',
-    lastName: 'X'
-  };
-  const func = jest.fn();
-  const wrapper = shallow(
-    <Programme id="1" programme={programme} getProgramme={func} updateProgramme={func} />
-  );
+  it('should render NotFound if there is no programme and not fetching', () => {
+    const wrapper = shallow(<Programme id="1" {...props} />);
 
-  expect(wrapper.contains(<h1>Dev X</h1>)).toBeTruthy();
+    expect(wrapper.find(NotFound)).toHaveLength(1);
+  });
+
+  it('should render programme form', () => {
+    const programme = {
+      name: 'D-Guild',
+      code: 'D'
+    };
+    const wrapper = shallow(
+      <Programme id="1" {...props} programme={programme} />
+    );
+
+    expect(wrapper.find(ProgrammeForm)).toBeTruthy();
+  });
+
+  // If there is not a programme in props we want to create a new one
+  it('updateProgramme functions correctly', () => {
+    const wrapper = shallow(<Programme id="1" {...props} />);
+    const newCode = 'D';
+    wrapper.instance().updateProgramme({ code: newCode });
+    expect(props.createProgramme).toHaveBeenCalledWith({
+      programme: { code: newCode }
+    });
+  });
+
+  // If there is a programme in props we want to update it
+  it('updateProgramme functions correctly', () => {
+    const programme = {
+      name: 'D-Guild',
+      code: 'F'
+    };
+
+    const id = '1';
+    const wrapper = shallow(
+      <Programme id={id} {...props} programme={programme} />
+    );
+    const newName = 'F-Guild';
+    wrapper.instance().updateProgramme({ name: newName });
+    expect(props.updateProgramme).toHaveBeenCalledWith(id, {
+      programme: { name: newName }
+    });
+  });
 });
