b4267311af5f1ffd45e4668d54e400604ef359b4
antongoransson <anton.goransson95@gmail.com>
2018-10-17 13:45:33 +0200

    Fix typo

--- priv/react_app/src/Screens/Session/SessionsApproved/SessionsApproved.js
+++ priv/react_app/src/Screens/Session/SessionsApproved/SessionsApproved.js
@@ -104,7 +104,7 @@ class StudentSessions extends Component {
         <h1>Student Sessions</h1>
         <h4>
           The time slots found on this page is the student sessions you have
-          been approved for. You have to confirme all the slots that you would
+          been approved for. You have to confirm all the slots that you would
           like to keep, otherwise, the slot will be given to another student.
         </h4>
         <List
