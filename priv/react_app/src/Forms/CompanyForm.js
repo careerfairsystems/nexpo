a02e59b2461f264cae28dcecfd624a9dbcebc749
saammirghorbani <95sami81@gmail.com>
2018-10-17 13:45:18 +0200

    UI for time slot generation

--- priv/react_app/src/Forms/CompanyForm.js
+++ priv/react_app/src/Forms/CompanyForm.js
@@ -3,21 +3,18 @@ import PropTypes from 'prop-types';
 import { Field, FieldArray, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { isNil } from 'lodash/fp';
-import { Button, Form, Input, Radio } from 'antd';
+import { Button, Form, Input, TimePicker, DatePicker } from 'antd';
+import moment from 'moment';
 import makeField, { required } from './helper';
 import UploadButton from './UploadButton';
 import DynamicTimeSlots from './DynamicTimeSlots';
 
-const plainOptions = [
-  { value: 0, label: 'No days' },
-  { value: 1, label: 'First day' },
-  { value: 2, label: 'Second day' },
-  { value: 3, label: 'Both days' }
-];
+const format = 'HH:mm';
+const dateFormat = 'YYYY/MM/DD';
 
+const InputGroup = Input.Group;
 const TextInput = makeField(Input);
 const TextArea = makeField(Input.TextArea);
-const RadioGroup = makeField(Radio.Group);
 
 const CompanyForm = ({ handleSubmit, onCancel, submitting }) => (
   <Form onSubmit={handleSubmit}>
@@ -43,18 +40,62 @@ const CompanyForm = ({ handleSubmit, onCancel, submitting }) => (
       required
     />
     <Field
-      name="studentSessionDays"
-      label="Student Session Days:"
-      options={plainOptions}
-      component={RadioGroup}
-    />
-    <Field
       name="logoUrl"
       label="Logo"
       accept=".jpg,.jpeg,.gif,.png"
       component={UploadButton}
     />
     <h3>Student Session Time Slots</h3>
+    <text>Student Session Days:</text>
+    <div>
+      <DatePicker
+        defaultValue={moment('2018/11/14', dateFormat)}
+        format={dateFormat}
+      />
+      <DatePicker
+        defaultValue={moment('2018/11/15', dateFormat)}
+        format={dateFormat}
+      />
+    </div>
+    <text>Student Session Timeframe:</text>
+    <div>
+      <TimePicker
+        name="starttime"
+        defaultValue={moment('08:00', format)}
+        format={format}
+      />
+      <TimePicker
+        name="endtime"
+        defaultValue={moment('16:00', format)}
+        format={format}
+      />
+    </div>
+
+    <InputGroup compact>
+      <Field
+        name="timeslot"
+        type="text"
+        component={TextInput}
+        label="Timeslot length"
+        addonAfter="minutes"
+      />
+
+      <Field
+        name="break"
+        type="text"
+        component={TextInput}
+        label="Break length"
+        addonAfter="minutes"
+      />
+      <Field
+        name="location"
+        type="text"
+        component={TextInput}
+        label="Location"
+      />
+    </InputGroup>
+
+    <Button type="secondary">Generate</Button>
     <FieldArray name="studentSessionTimeSlots" component={DynamicTimeSlots} />
     {onCancel && <Button onClick={onCancel}>Cancel</Button>}
     <Button disabled={submitting} htmlType="submit" type="primary">
