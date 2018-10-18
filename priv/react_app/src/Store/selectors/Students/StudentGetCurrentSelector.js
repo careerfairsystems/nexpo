8b3f4890f404166403bceaa5aa504d758ba79357
VictorWinberg <dat14vwi@student.lu.se>
2018-10-18 11:24:00 +0200

    Add comment

--- priv/react_app/src/Store/selectors/Students/StudentGetCurrentSelector.js
+++ priv/react_app/src/Store/selectors/Students/StudentGetCurrentSelector.js
@@ -2,6 +2,8 @@ import users from '../Users';
 
 export const getCurrentStudent = state => {
   const { student = {} } = users.getCurrentUser(state);
+  // TODO: This is kinda weird but I want programmes to be flat and not object
+  // Not sure if we always want flat object or nested object
   const { programme } = state.entities.students[student.id];
 
   return { ...student, programme };
