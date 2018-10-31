325325f2b1f3b89952b9b35fe60c4282ca394ed4
Victor Winberg <victor.m.winberg@gmail.com>
2018-10-31 19:40:35 +0100

    Fix frontend tests

--- priv/react_app/src/Store/reducers/Entities/EntitiesReducer.test.js
+++ priv/react_app/src/Store/reducers/Entities/EntitiesReducer.test.js
@@ -19,8 +19,9 @@ describe('Entities reducer', () => {
       programmes: {},
       roles: {},
       users: {},
-      studentSessionApplications: {},
       studentSessions: {},
+      studentSessionTimeSlots: {},
+      studentSessionApplications: {},
       statistics: {},
       students: {}
     };
