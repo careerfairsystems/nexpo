9e72899169ef046e113ed8eee57883a9f6bbb595
Victor Winberg <victor.m.winberg@gmail.com>
2018-11-01 13:33:24 +0100

    Add bulk create sessions

--- web/controllers/student_session_controller.ex
+++ web/controllers/student_session_controller.ex
@@ -8,7 +8,7 @@ defmodule Nexpo.StudentSessionController do
   plug EnsurePermissions, [handler: Nexpo.SessionController,
                            one_of: [%{default: ["write_all"]},
                                     %{default: ["write_companies"]}]
-                          ] when action in [:create, :delete]
+                          ] when action in [:create, :create_bulk, :delete]
 
   def create(conn, %{"student_session" => student_sessions_params}, _user, _claims) do
     company = Repo.get(Company, student_sessions_params["company_id"])
@@ -19,14 +19,14 @@ defmodule Nexpo.StudentSessionController do
       order_by: [desc: appl.score, asc: student.id],
       left_join: session in StudentSession,
       on: student.id == session.student_id and session.company_id == ^company.id,
-      left_join: slot in assoc(session, :student_session_time_slot),
-      where: is_nil(slot.id) or slot.company_id != ^company.id,
+      where: is_nil(session.id),
       limit: 1,
       select: student)
 
     case student do
       nil -> conn
-        |> redirect(to: company_path(conn, :show, company))
+        |> put_status(404)
+        |> render(ErrorView, "404.json")
       student ->
         data = Map.put(student_sessions_params, "student_id", student.id)
         changeset = StudentSession.changeset(%StudentSession{}, data)
@@ -36,6 +36,73 @@ defmodule Nexpo.StudentSessionController do
             conn
             |> redirect(to: company_path(conn, :show, company))
           {:error, changeset} ->
+            conn
+            |> put_status(:unprocessable_entity)
+            |> render(Nexpo.ChangesetView, "error.json", changeset: changeset)
+        end
+    end
+  end
+
+  def create_bulk(conn, %{"student_sessions" => student_sessions_params}, _user, _claims) do
+    company = Repo.get(Company, student_sessions_params["company_id"])
+
+    time_slots = Repo.all(
+      from slot in Ecto.assoc(company, :student_session_time_slots),
+      left_join: session in assoc(slot, :student_session),
+      where: is_nil(session.id) or session.student_confirmed != true)
+
+    students = Repo.all(
+      from appl in Ecto.assoc(company, :student_session_applications),
+      join: student in assoc(appl, :student),
+      order_by: [desc: appl.score, asc: student.id],
+      left_join: session in StudentSession,
+      on: student.id == session.student_id and session.company_id == ^company.id,
+      where: is_nil(session.id),
+      limit: ^length(time_slots),
+      select: student)
+
+    case students do
+      [] -> conn
+        |> put_status(404)
+        |> render(Nexpo.ErrorView, "404.json")
+
+      students ->
+        delete_bulk = time_slots
+        |> Repo.preload(:student_session)
+        |> Enum.filter(fn time_slot -> not is_nil(time_slot.student_session) end)
+        |> Enum.map(fn time_slot ->
+          Repo.get!(StudentSession, time_slot.student_session.id)
+        end)
+        |> Enum.with_index()
+        |> Enum.reduce(Ecto.Multi.new(), fn ({changeset, index}, multi) ->
+          Ecto.Multi.delete(multi, Integer.to_string(index), changeset)
+        end)
+
+        insert_bulk = time_slots
+        |> Enum.shuffle
+        |> Enum.zip(students)
+        |> Enum.map(fn {time_slot, student} ->
+          data = student_sessions_params
+          |> Map.put("student_session_time_slot_id", time_slot.id)
+          |> Map.put("student_id", student.id)
+
+          StudentSession.changeset(%StudentSession{}, data)
+        end)
+        |> Enum.with_index()
+        |> Enum.reduce(Ecto.Multi.new(), fn ({changeset, index}, multi) ->
+          Ecto.Multi.insert(multi, Integer.to_string(index + length(delete_bulk.operations)), changeset)
+        end)
+
+        result = delete_bulk
+        |> Ecto.Multi.append(insert_bulk)
+        |> Repo.transaction
+
+        case result do
+          {:ok, _} ->
+            conn
+            |> put_status(303) # Status Code 303 See Other, that uses GET
+            |> redirect(to: company_path(conn, :show, company))
+          {:error, _, changeset, _ } ->
             conn
             |> put_status(:unprocessable_entity)
             |> render(Nexpo.ChangesetView, "error.json", changeset: changeset)
