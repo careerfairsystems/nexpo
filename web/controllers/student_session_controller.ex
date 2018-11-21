0178fffe9f21a8148b73b5f9da48c7ed3acbab6e
Victor Winberg <victor.m.winberg@gmail.com>
2018-11-21 12:52:52 +0100

    [StudentSession] Update create_bulk to all companies (#299)
    * Update create_bulk to all companies
    
    * Remove unused variables

--- web/controllers/student_session_controller.ex
+++ web/controllers/student_session_controller.ex
@@ -41,101 +41,96 @@ defmodule Nexpo.StudentSessionController do
     end
   end
 
-  def create_bulk(conn, %{"student_sessions" => student_sessions_params}, _user, _claims) do
-    company = Repo.get(Company, student_sessions_params["company_id"])
+  def create_bulk(conn, %{}, _user, _claims) do
+    Repo.all(
+      from company in Company,
+      join: slot in assoc(company, :student_session_time_slots),
+      group_by: company.id)
+    |> Enum.each(fn company ->
+      time_slots = Repo.all(
+        from slot in Ecto.assoc(company, :student_session_time_slots),
+        left_join: session in assoc(slot, :student_session),
+        where: is_nil(session.id))
 
-    time_slots = Repo.all(
-      from slot in Ecto.assoc(company, :student_session_time_slots),
-      left_join: session in assoc(slot, :student_session),
-      where: is_nil(session.id) or session.student_confirmed != true)
+      student_ids = Repo.all(
+        from appl in Ecto.assoc(company, :student_session_applications),
+        join: student in assoc(appl, :student),
+        where: not is_nil(appl.score) and appl.score > 0,
+        order_by: [desc: appl.score, asc: student.id],
+        # Check that student does not already have session with given company
+        left_join: session in StudentSession,
+        on: student.id == session.student_id and session.company_id == ^company.id,
+        where: is_nil(session.id),
+        limit: ^length(time_slots),
+        select: student.id)
 
-    student_ids = Repo.all(
-      from appl in Ecto.assoc(company, :student_session_applications),
-      join: student in assoc(appl, :student),
-      where: not is_nil(appl.score) and appl.score > 0,
-      order_by: [desc: appl.score, asc: student.id],
-      # Check that student does not already have session with given company
-      left_join: session in StudentSession,
-      on: student.id == session.student_id and session.company_id == ^company.id,
-      where: is_nil(session.id),
-      limit: ^length(time_slots),
-      select: student.id)
+      students = Repo.all(
+        from student in Student,
+        where: student.id in ^student_ids,
+        left_join: session in assoc(student, :student_sessions),
+        group_by: student.id,
+        order_by: count(session.id),
+        select: student)
 
-    students = Repo.all(
-      from student in Student,
-      where: student.id in ^student_ids,
-      left_join: session in assoc(student, :student_sessions),
-      group_by: student.id,
-      order_by: count(session.id),
-      select: student)
+      if not Enum.empty?(students) do
+          result = students
+          |> greedy_assign(time_slots, company, 5)
+          |> Enum.with_index()
+          |> Enum.reduce(Ecto.Multi.new(), fn ({changeset, index}, multi) ->
+            Ecto.Multi.insert(multi, Integer.to_string(index), changeset)
+          end)
+          |> Repo.transaction
 
-    case students do
-      [] -> conn
-        |> put_status(404)
-        |> render(Nexpo.ErrorView, "404.json")
-
-      students ->
-        delete_bulk = time_slots
-        |> Repo.preload(:student_session)
-        |> Enum.filter(fn time_slot -> not is_nil(time_slot.student_session) end)
-        |> Enum.map(fn time_slot ->
-          Repo.get!(StudentSession, time_slot.student_session.id)
-        end)
-        |> Enum.with_index()
-        |> Enum.reduce(Ecto.Multi.new(), fn ({changeset, index}, multi) ->
-          Ecto.Multi.delete(multi, Integer.to_string(index), changeset)
-        end)
-
-        insert_bulk = students
-        |> Enum.reduce({[], Enum.shuffle(time_slots)}, fn student, {acc, slots} ->
-          case Enum.find_index(slots, fn time_slot ->
-            case Repo.all(
-              from session in Ecto.assoc(student, :student_sessions),
-              # Check that student does not already have session at the time of the given time slot
-              left_join: slot in TimeSlot,
-              on: slot.id == session.student_session_time_slot_id and
-                  slot.start == ^time_slot.start and slot.end == ^time_slot.end,
-              where: not is_nil(slot.id),
-              select: slot
-            ) do
-              [] -> true
-              _ -> false
-            end
-          end) do
-            nil -> conn
-              |> put_status(404)
-              |> render(Nexpo.ErrorView, "404.json")
-            index ->
-              {time_slot, new_slots} = List.pop_at(slots, index)
-              data = student_sessions_params
-              |> Map.put("student_session_time_slot_id", time_slot.id)
-              |> Map.put("student_id", student.id)
-
-              new_acc = [StudentSession.changeset(%StudentSession{}, data) | acc]
-              {new_acc, new_slots}
-          end
-        end)
-        |> elem(0)
-        |> Enum.with_index()
-        |> Enum.reduce(Ecto.Multi.new(), fn ({changeset, index}, multi) ->
-          Ecto.Multi.insert(multi, Integer.to_string(index + length(delete_bulk.operations)), changeset)
-        end)
-
-        result = delete_bulk
-        |> Ecto.Multi.append(insert_bulk)
-        |> Repo.transaction
-
-        case result do
-          {:ok, _} ->
-            conn
-            |> put_status(303) # Status Code 303 See Other, that uses GET
-            |> redirect(to: company_path(conn, :show, company))
-          {:error, _, changeset, _ } ->
+          with {:error, _, changeset, _ } <- result do
             conn
             |> put_status(:unprocessable_entity)
             |> render(Nexpo.ChangesetView, "error.json", changeset: changeset)
-        end
+          end
+      end
+    end)
+
+    student_sessions = Repo.all(StudentSession)
+    render(conn, "index.json", student_sessions: student_sessions)
+  end
+
+  defp greedy_assign(_students, _time_slots, _company, tries) when tries <= 0 do [] end
+  defp greedy_assign(students, time_slots, company, tries) when tries > 0 do
+    case get_student_sessions(students, time_slots, company) do
+      [] -> greedy_assign(students, time_slots, company, tries - 1)
+      sessions -> sessions
     end
+  end
+
+  defp get_student_sessions(students, time_slots, company) do
+    Enum.reduce(students, {[], Enum.shuffle(time_slots)}, fn student, {acc, slots} ->
+      case Enum.find_index(slots, fn time_slot ->
+        case Repo.all(
+          from session in Ecto.assoc(student, :student_sessions),
+          # Check that student does not already have session at the time of the given time slot
+          left_join: slot in TimeSlot,
+          on: slot.id == session.student_session_time_slot_id and
+              slot.start == ^time_slot.start and slot.end == ^time_slot.end,
+          where: not is_nil(slot.id),
+          select: slot
+        ) do
+          [] -> true
+          _ -> false
+        end
+      end) do
+        nil -> {[], []}
+        index ->
+          {time_slot, new_slots} = List.pop_at(slots, index)
+          data = %{
+            student_session_time_slot_id: time_slot.id,
+            student_id: student.id,
+            company_id: company.id
+          }
+
+          new_acc = [StudentSession.changeset(%StudentSession{}, data) | acc]
+          {new_acc, new_slots}
+      end
+    end)
+    |> elem(0)
   end
 
   def show_reserves(conn, %{}, _user, _claims) do
