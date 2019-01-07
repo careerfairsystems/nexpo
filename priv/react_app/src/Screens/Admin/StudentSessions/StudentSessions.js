640921248490839a2cdb745663645d7f9ab635f8
Victor Winberg <victor.m.winberg@gmail.com>
2019-01-07 11:04:19 +0100

    Update Admin StudentSession

--- priv/react_app/src/Screens/Admin/StudentSessions/StudentSessions.js
+++ priv/react_app/src/Screens/Admin/StudentSessions/StudentSessions.js
@@ -1,51 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Popconfirm } from 'antd';
-import LoadingSpinner from '../../../Components/LoadingSpinner';
 import HtmlTitle from '../../../Components/HtmlTitle';
 
 import API from '../../../API';
 
-/**
- * Responsible for rendering a list of companies
- */
 type Props = {
-  companies?: {},
   createBulkStudentSessions: () => Promise<void>
 };
-class StudentSessions extends Component<Props> {
-  static defaultProps = {
-    companies: {}
-  };
 
-  render() {
-    return (
-      <div>
-        <HtmlTitle title="StudentSessions" />
+const StudentSessions = ({ createBulkStudentSessions }: Props) => (
+  <div>
+    <HtmlTitle title="StudentSessions" />
 
-        <h1>Student Session</h1>
-        <Popconfirm
-          title="Sure to assign empty and non-confirmed time slots?"
-          onConfirm={() => {
-            const { createBulkStudentSessions } = this.props;
-            createBulkStudentSessions();
-          }}
-        >
-          <Button onClick={() => null}>Assign all</Button>
-        </Popconfirm>
-        <br />
-        <Popconfirm
-          title="Sure to delete all non confirmed student sessions?"
-          onConfirm={API.studentSessions.deleteNonConfirmed}
-        >
-          <Button onClick={() => null}>Remove non-confirmed sessions</Button>
-        </Popconfirm>
-        <br />
-        <Button icon="download" onClick={API.studentSessions.downloadReserves}>
-          Download reserves
-        </Button>
-      </div>
-    );
-  }
-}
+    <h1>Student Session</h1>
+    <Popconfirm
+      title="Sure to assign empty and non-confirmed time slots?"
+      onConfirm={createBulkStudentSessions}
+    >
+      <Button onClick={() => null}>Assign all</Button>
+    </Popconfirm>
+    <br />
+    <Popconfirm
+      title="Sure to delete all non confirmed student sessions?"
+      onConfirm={API.studentSessions.deleteNonConfirmed}
+    >
+      <Button onClick={() => null}>Remove non-confirmed sessions</Button>
+    </Popconfirm>
+    <br />
+    <Button icon="download" onClick={API.studentSessions.downloadReserves}>
+      Download reserves
+    </Button>
+  </div>
+);
 
 export default StudentSessions;
