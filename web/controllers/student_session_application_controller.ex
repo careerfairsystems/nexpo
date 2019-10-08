888726fbd8937716b52276306bd65caa0732bb62
Viktor Claesson <claesson.viktor@gmail.com>
2019-10-08 04:59:51 +0200

    editing stu sesh appl now works

--- web/controllers/student_session_application_controller.ex
+++ web/controllers/student_session_application_controller.ex
@@ -2,7 +2,7 @@ defmodule Nexpo.StudentSessionApplicationController do
   use Nexpo.Web, :controller
   use Guardian.Phoenix.Controller
 
-  alias Nexpo.{Student, Representative, StudentSessionApplication}
+  alias Nexpo.{Student, StudentSessionApplication}
 
   def create(
         conn,
@@ -33,37 +33,19 @@ defmodule Nexpo.StudentSessionApplicationController do
 
   def update_me(
         conn,
-        %{"id" => id, "student_session_application" => student_session_applications_params},
+        %{
+          "id" => application_id,
+          "student_session_application" => student_session_applications_params
+        },
         user,
         _claims
       ) do
-    # case Repo.get_by(Student, %{user_id: user.id}) do
-    #   nil -> nil
-    #   student ->
-    #     case Repo.get_by(StudentSessionApplication, %{id: id, student_id: student.id}) do
-    #       nil ->  conn
-    #         |> put_status(400)
-    #         |> render(Nexpo.ErrorView, "400.json")
-    #       application ->
-    #         changeset = StudentSessionApplication.student_changeset(application, student_session_applications_params)
-    #         case Repo.update(changeset) do
-    #           {:ok, appl} ->
-    #             render(conn, "show.json", student_session_application: appl)
-    #           {:error, changeset} ->
-    #             conn
-    #             |> put_status(:unprocessable_entity)
-    #             |> render(Nexpo.ChangesetView, "error.json", changeset: changeset)
-    #         end
-    #     end
-    # end
-    case Repo.get_by(Representative, %{user_id: user.id}) do
+    case Repo.get_by(Student, %{user_id: user.id}) do
       nil ->
         nil
 
-      representative ->
-        company = Ecto.assoc(representative, :company) |> Repo.one()
-
-        case Repo.get_by(StudentSessionApplication, %{id: id, company_id: company.id}) do
+      student ->
+        case Repo.get_by(StudentSessionApplication, %{id: application_id, student_id: student.id}) do
           nil ->
             conn
             |> put_status(400)
@@ -71,7 +53,7 @@ defmodule Nexpo.StudentSessionApplicationController do
 
           application ->
             changeset =
-              StudentSessionApplication.representative_changeset(
+              StudentSessionApplication.student_changeset(
                 application,
                 student_session_applications_params
               )
@@ -89,14 +71,14 @@ defmodule Nexpo.StudentSessionApplicationController do
     end
   end
 
-  def delete_me(conn, %{"id" => id}, user, _claims) do
+  def delete_me(conn, %{"id" => application_id}, user, _claims) do
     student = Repo.get_by!(Student, %{user_id: user.id})
 
-    case Repo.get_by(StudentSessionApplication, %{id: id, student_id: student.id}) do
+    case Repo.get_by(StudentSessionApplication, %{id: application_id, student_id: student.id}) do
       nil ->
         conn
-        |> put_status(400)
-        |> render(Nexpo.ErrorView, "400.json")
+        |> put_status(404)
+        |> render(Nexpo.ErrorView, "404.json")
 
       application ->
         Repo.delete!(application)
