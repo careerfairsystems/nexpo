cbdfe2962397bbd41006d40138dda955adb70ea2
Filip Hedén <filipheden@gmail.com>
2019-10-21 18:38:29 +0200

    Now actually compiles too!

--- priv/repo/migrations/20191021162244_add_to_company_table.exs
+++ priv/repo/migrations/20191021162244_add_to_company_table.exs
@@ -2,9 +2,10 @@ defmodule Nexpo.Repo.Migrations.AddToCompanyTable do
   use Ecto.Migration
 
   def change do
-    alter table(:company) do
-      add(:host_name)
-      add(:host_mail)
-      add(:host_phone_number)
+    alter table(:companies) do
+      add(:host_name, :string)
+      add(:host_mail, :string)
+      add(:host_phone_number, :string)
+    end
   end
 end
